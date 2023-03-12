8df362a48bb341219070f8e6f083eb7bd2e27ae7
Mark <remarkablemark@users.noreply.github.com>
2023-03-12 19:37:44 -0400

    feat(functions): add graphql
    https://developers.cloudflare.com/pages/platform/functions/plugins/graphql/

---
+++ functions/graphql.ts
@@ -0,0 +1,27 @@
+// https://developers.cloudflare.com/pages/platform/functions/plugins/graphql/
+import graphQLPlugin from "@cloudflare/pages-plugin-graphql";
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return "Hello, world!";
+        },
+      },
+    },
+  }),
+});
+
+export const onRequest: PagesFunction = graphQLPlugin({
+  schema,
+  graphql,
+});

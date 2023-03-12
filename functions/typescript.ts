6e88f588ba61598cdeed8f13a9624ed1d91749a5
Mark <mark@remarkablemark.org>
2023-03-12 18:12:51 -0400

    feat(functions): add typescript
    https://developers.cloudflare.com/pages/platform/functions/typescript/

---
+++ functions/typescript.ts
@@ -0,0 +1,6 @@
+// https://developers.cloudflare.com/pages/platform/functions/typescript/
+interface Env {}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  return new Response('Hello, TypeScript!');
+};

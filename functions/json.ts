6e3d00454a0073a00da396ac258ed4658be14151
Mark <mark@remarkablemark.org>
2023-03-12 18:20:59 -0400

    feat(functions): add json
    https://developers.cloudflare.com/workers/examples/return-json/

---
+++ functions/json.ts
@@ -0,0 +1,14 @@
+// https://developers.cloudflare.com/workers/examples/return-json/
+export const onRequest: PagesFunction = async (context) => {
+  const data = {
+    hello: 'world',
+  };
+
+  const json = JSON.stringify(data, null, 2);
+
+  return new Response(json, {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  });
+};

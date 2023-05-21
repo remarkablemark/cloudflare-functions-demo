34e6f01be8e543c5a3720e801b4ebc6dc233d685
Mark <mark@remarkablemark.org>
2023-05-21 18:28:51 -0400

    feat(functions): add querystring

---
+++ functions/querystring.ts
@@ -0,0 +1,25 @@
+interface Env {}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const url = new URL(context.request.url);
+
+  const params = {};
+
+  url.searchParams.forEach((value, key) => {
+    if (params[key] === undefined) {
+      params[key] = value;
+      return;
+    }
+
+    if (Array.isArray(params[key])) {
+      params[key].push(value);
+      return;
+    }
+
+    params[key] = [params[key], value];
+  });
+
+  return new Response(JSON.stringify(params), {
+    headers: { 'content-type': 'application/json' },
+  });
+};

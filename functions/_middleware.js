7e5c8da6603169f595b516cef6de8349c3d95d27
Mark <mark@remarkablemark.org>
2023-05-24 19:18:13 -0400

    chore(functions): tidy JSDoc in _middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,4 +1,6 @@
-// https://developers.cloudflare.com/pages/platform/functions/middleware/#add-middleware
+/**
+ * @see {@link https://developers.cloudflare.com/pages/platform/functions/middleware/#add-middleware}
+ */
 async function errorHandling(context) {
   try {
     return await context.next();

49c464a8525e90b2513f52c726997ef64579bd6d
Mark <mark@remarkablemark.org>
2023-05-25 19:34:01 -0400

    feat(functions): add cache request example
    https://developers.cloudflare.com/workers/examples/cache-post-request/

---
+++ functions/cache.ts
@@ -0,0 +1,54 @@
+/**
+ * @see {@link https://developers.cloudflare.com/workers/examples/cache-post-request/}
+ */
+
+const requestUrl = 'http://example.com';
+
+export const onRequestGet: PagesFunction = async (context) => {
+  try {
+    const body = await context.request.clone().text();
+
+    // Hash the request body to use it as a part of the cache key
+    const hash = await sha256(body);
+    const cacheUrl = new URL(context.request.url);
+
+    // Store the URL in cache by prepending the body's hash
+    cacheUrl.pathname = '/posts' + cacheUrl.pathname + hash;
+
+    // Convert to a GET to be able to cache
+    const cacheKey = new Request(cacheUrl.toString(), {
+      headers: context.request.headers,
+      method: 'GET',
+    });
+
+    // Find the cache key in the cache
+    const cache = caches.default;
+    let response = await cache.match(cacheKey);
+
+    // Otherwise, fetch response to GET request from origin
+    if (response) {
+      console.log('Cache hit');
+      return response;
+    } else {
+      console.log('Cache miss');
+      response = await fetch(requestUrl);
+      context.waitUntil(cache.put(cacheKey, response.clone()));
+      return response;
+    }
+  } catch (error) {
+    return new Response('Error thrown: ' + error.message);
+  }
+};
+
+async function sha256(message: string): Promise<string> {
+  // encode as UTF-8
+  const msgBuffer = await new TextEncoder().encode(message);
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+  // convert bytes to hex string
+  return [...new Uint8Array(hashBuffer)]
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
+}

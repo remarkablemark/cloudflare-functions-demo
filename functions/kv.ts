54852627ba8692dd353444b27dacd82b3202c94a
Mark <mark@remarkablemark.org>
2023-05-24 21:28:51 -0400

    feat(functions): add kv
    https://developers.cloudflare.com/pages/platform/functions/bindings/#kv-namespaces

---
+++ functions/kv.ts
@@ -0,0 +1,33 @@
+/**
+ * @see {@link https://developers.cloudflare.com/pages/platform/functions/bindings/#kv-namespaces}
+ */
+interface Env {
+  MY_KV_BINDING: KVNamespace;
+}
+
+const html = (key, value) => `
+<form method="post">
+  <label>${key}:
+    <input name="${key}" value="${value || ''}">
+  </label>
+  <input type="submit" value="Set">
+</form>
+`;
+
+const KEY = 'KEY';
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  const value = await context.env.MY_KV_BINDING.get(KEY);
+  return new Response(html(KEY, value), {
+    headers: { 'content-type': 'text/html' },
+  });
+};
+
+export const onRequestPost: PagesFunction<Env> = async (context) => {
+  const formData = await context.request.formData();
+  const value = formData.get(KEY);
+  await context.env.MY_KV_BINDING.put(KEY, value);
+  return new Response(html(KEY, value), {
+    headers: { 'content-type': 'text/html' },
+  });
+};

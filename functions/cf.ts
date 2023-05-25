0f8253cbc39167e4f13f7e35a798d268f46bb499
Mark <mark@remarkablemark.org>
2023-05-25 18:01:16 -0400

    docs(functions): add link to cloudflare object example
    https://developers.cloudflare.com/workers/examples/accessing-the-cloudflare-object/

--- functions/cf.ts
+++ functions/cf.ts
@@ -1,5 +1,6 @@
 /**
  * @see {@link https://developers.cloudflare.com/workers/runtime-apis/request/#incomingrequestcfproperties}
+ * @see {@link https://developers.cloudflare.com/workers/examples/accessing-the-cloudflare-object/}
  */
 export const onRequest: PagesFunction = async (context) => {
   return new Response(JSON.stringify(context.request.cf, null, 2), {

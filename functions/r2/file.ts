8e5d7517561c99b067ab17bc7b01d4c555feb902
Mark <mark@remarkablemark.org>
2023-05-28 23:18:50 -0400

    feat(functions): improve R2 example

--- functions/r2/file.ts
+++ functions/r2/file.ts
@@ -1,7 +1,14 @@
 import { Env, KEY } from './shared';
 
+/**
+ * @see {@link https://developers.cloudflare.com/workers/learning/using-streams/}
+ */
 export const onRequestGet: PagesFunction<Env> = async (context) => {
   const obj = await context.env.R2_BUCKET.get(KEY);
   const fileData = await obj.arrayBuffer();
-  return new Response(fileData);
+  return new Response(fileData, {
+    headers: {
+      'Content-Disposition': 'attachment',
+    },
+  });
 };
--- functions/r2/index.ts
+++ functions/r2/index.ts
@@ -1,11 +1,17 @@
 import { Env, KEY } from './shared';
 
-const getHtml = `
+const html = `
 <form method="post" enctype="multipart/form-data" action="r2/upload">
   <input type="file" name="${KEY}">
   <br><br>
-  <input type="Submit" value="Upload">
+  <input disabled type="submit" value="Upload">
 <form>
+<script>
+  document.querySelector('input[type="file"]').addEventListener('change', () => {
+    document.querySelector('input[type="submit"]').disabled = false;
+  });
+</script>
+
 <br><br>
 <a href="r2/file" download>
   <img src="r2/file">
@@ -16,9 +22,7 @@ const getHtml = `
  * @see {@link https://developers.cloudflare.com/pages/platform/functions/bindings/#r2-buckets}
  */
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-  const obj = await context.env.R2_BUCKET.get(KEY);
-  const fileData = await obj.arrayBuffer();
-  return new Response(getHtml, {
+  return new Response(html, {
     headers: { 'content-type': 'text/html' },
   });
 };
